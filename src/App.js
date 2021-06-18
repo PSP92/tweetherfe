8ceb34ea84a8b25a7247a4a0fd956c89986bdaaa
PSP92 <78216088+PSP92@users.noreply.github.com>
2021-06-18 09:28:47 -0500

    added routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import {Route} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
       <h1>TweetHER</h1>
+      <Route path ="/login"/>
+      <Route path="/home"/>
+      <Route path="/search"/>
+      <Route path="/notifications"/>
+      <Route path="/directmessage"/>
+      <Route path="/profile"/>
+
+        
     </div>
   );
 }
